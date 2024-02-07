b8b31fd9781c7f85611bcb96f7881b2c513e0d3b
GitHudsonLima <hudsonoliveiral@outlook.com>
2024-02-07 10:08:24 -0500

    built test

--- reactjs-nextjs-project-1/src/index.js
+++ reactjs-nextjs-project-1/src/index.js
@@ -4,13 +4,12 @@ import "./styles/global-styles.css";
 import reportWebVitals from "./reportWebVitals";
 import { Home } from "./templates/Home";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
